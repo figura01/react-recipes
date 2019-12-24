7872e205bee78180eb72e76e03ae5731232a9200
cecile <cecile@snowflake>
2019-12-24 14:39:40 +0100

    Bonus spécial

--- src/components/Posts/SinglePost.js
+++ src/components/Posts/SinglePost.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SinglePost = ({ post }) => (
-  <article className="post">
+  <article className="post post-single">
     <h2 className="post-title">{post.title}</h2>
     <h3 className="post-category">{post.category}</h3>
     <p className="post-content">{post.content}</p>
