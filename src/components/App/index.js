50f897909de6a21bfbcdaaf88029810febb38abd
cecile <cecile@snowflake>
2019-12-24 11:07:46 +0100

    On gère les clics sur le header avec des Link

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -14,47 +14,19 @@ import categoriesData from 'src/data/categories';
 import postsData from 'src/data/posts';
 
 // == Composant
-// Une classe pour gérer du state
-class App extends React.Component {
-  constructor(props) {
-    // On appelle le constructor du parent React.Component
-    super(props);
-
-    // On veut transmettre / fournir la valeur de this (objet / instance courante)
-    // à nos méthodes persos (handleClick, handle....)
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-
-  // function handleClickOnDocument() { .... }
-  // document.addEventListener('click', function() { console.log('Je click'); });
-  // Agir sur le state (setState)
-  handleClick(category) {
-    console.log('Je click depuis la méthode de App');
-  }
-
-  // Rendu du composant
-  render() {
-    // const filteredPostsReact = filterPostsByCategory(postsData, categoriesData[2]);
-    // const filteredPostsAngular = filterPostsByCategory(postsData, categoriesData[1]);
-    // console.log(filteredPosts);
-
-    // Route : si on ne met pas "exact", la comparaison est "le chemin commence par ..."
-    // avec exact : "le chemin est exactement ..."
-
-    return (
-      <div id="blog">
-        <Header categories={categoriesData} onCategoryClick={this.handleClick} />
-        {categoriesData.map((category) => (
-          <Route key={category.label} exact path={category.route}>
-            <Posts posts={filterPostsByCategory(postsData, category)} />
-          </Route>
-        ))}
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = () => (
+  // Route : si on ne met pas "exact", la comparaison est "le chemin commence par ..."
+  // avec exact : "le chemin est exactement ..."
+  <div id="blog">
+    <Header categories={categoriesData} />
+    {categoriesData.map((category) => (
+      <Route key={category.label} exact path={category.route}>
+        <Posts posts={filterPostsByCategory(postsData, category)} />
+      </Route>
+    ))}
+    <Footer />
+  </div>
+);
 
 // == Export
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,29 @@
 // == import : npm
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 // == import : local
 import './header.scss';
 
 // == Composant
-const Header = ({ categories, onCategoryClick }) => (
+const Header = ({ categories }) => (
   <header>
     <nav className="nav">
       {categories.map((category) => (
-        <a
-          onClick={onCategoryClick(category)}
-          className="nav-link"
+        <Link
+          to={category.route}
           key={category.label}
+          className="nav-link"
         >
           {category.label}
-        </a>
+        </Link>
       ))}
     </nav>
   </header>
 );
 
 Header.propTypes = {
-  onCategoryClick: PropTypes.func.isRequired,
   categories: PropTypes.arrayOf(
     PropTypes.shape({
       route: PropTypes.string.isRequired,
